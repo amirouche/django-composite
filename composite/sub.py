cf79502a0ddeda8c29461b05998f4690f4579932
Amirouche Boubekki <amirouche.boubekki@gmail.com>
2012-12-19 00:29:11 +0100

    Sub.urls() is now returning something useful for Django

--- composite/sub.py
+++ composite/sub.py
@@ -3,6 +3,10 @@ from django.conf.urls import patterns, url, include
 
 class Sub(object):
 
+    application_namespace = None
+    instance_namespace = None
+    name = None
+
     def __init__(self):
         self.resources = []
 
@@ -11,17 +15,18 @@ class Sub(object):
         self.resources.append(resource)
 
     def urls(self):
+        print 'self', self
         urls = []
         for resource in self.resources:
+            print 'resource', resource
             try:  # it might be another sub
                 urls.append(url(resource.path, include(resource.urls())))
-            except AttributeError, e:  # it is not
-                print self, resource, e
+            except AttributeError:  # it is not
                 try:  # it might have options
                     urls.append(url(resource.path, resource.as_view(), resource.options, resource.name))
                 except AttributeError:  # it has not
                     urls.append(url(resource.path, resource.as_view(), name=resource.name))
         urlpatterns = patterns('', *urls)
-        return urlpatterns, self.app_name, self.name
+        return urlpatterns, self.application_namespace, self.instance_namespace
 
 # That is all :)
