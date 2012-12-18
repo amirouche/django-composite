addf6670adca1482d7be2442d644c67156368225
Amirouche Boubekki <amirouche.boubekki@gmail.com>
2012-12-18 13:36:39 +0100

    attempt to get the urls right

--- composite/sub.py
+++ composite/sub.py
@@ -7,18 +7,21 @@ class Sub(object):
         self.resources = []
 
     def register(self, resource):
+        resource.sub = self
         self.resources.append(resource)
 
     def urls(self):
-        urls = list()
+        urls = []
         for resource in self.resources:
             try:  # it might be another sub
-                pattern = (resource.path, include(resource.urls()))
-            except:  # it is not
+                urls.append(url(resource.path, include(resource.urls())))
+            except AttributeError, e:  # it is not
+                print self, resource, e
                 try:  # it might have options
-                    pattern = (resource.path, resource, resource.options, resource.name)
-                except:  # it has not
-                    pattern = (resource.path, resource, name=resource.name)
-
+                    urls.append(url(resource.path, resource.as_view(), resource.options, resource.name))
+                except AttributeError:  # it has not
+                    urls.append(url(resource.path, resource.as_view(), name=resource.name))
+        urlpatterns = patterns('', *urls)
+        return urlpatterns, self.app_name, self.name
 
 # That is all :)
