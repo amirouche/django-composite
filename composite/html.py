83b7a926c1263e0bddec140dafc61b726a4eebe9
Amirouche Boubekki <amirouche.boubekki@gmail.com>
2012-12-25 09:42:10 +0100

    added html.Link widget

--- composite/html.py
+++ composite/html.py
@@ -1,13 +1,16 @@
+from django.core.urlresolvers import reverse
+
 from widget import Widget
 
 
 class Tag(Widget):
 
-    def __init__(self, name, widget_id=None, classes=None, *widgets):
+    def __init__(self, name, widget_id=None, classes=None, attributes=None, *widgets):
         super(Tag, self).__init__(widget_id)
         self.widgets = widgets
         self.classes = classes
         self.name = name
+        self.attributes = attributes
 
     def render(self, request, *args, **kwargs):
         # render opening tag
@@ -17,6 +20,9 @@ class Tag(Widget):
         if self.classes:
             classes = ' '.join(self.classes)
             widget += ' class="%s"' % classes
+        if self.attributes:
+            for key, value in self.attributes.items():
+                widget += ' %s="%s"' % (key, value)
         # render subwigets and closing tag
         subwidgets = self.render_subwidgets(request, *args, **kwargs)
         widget += '>%s</%s>' % (subwidgets, self.name)
@@ -36,3 +42,13 @@ class String(Widget):
 
     def render(self, request, *args, **kwargs):
         return self.string
+
+
+class Link(Tag):
+
+    def __init__(self, url, widget_id=None, classes=None, extra_attributes=None, *widgets):
+        if not url.startswith('#'):
+            url = reverse(url)
+        attributes = dict(href=url)
+        attributes.update(extra_attributes)
+        super(Link, self).__init__('a', widget_id, classes, attributes, *widgets)
